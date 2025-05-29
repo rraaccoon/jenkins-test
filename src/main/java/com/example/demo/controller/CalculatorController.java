e65fa27b93819479050eef89270030186e6b17a6
raccoon <144964272+rraaccoon@users.noreply.github.com>
2025-05-29 22:52:46 +0900

    []89

--- src/main/java/com/example/demo/controller/CalculatorController.java
+++ src/main/java/com/example/demo/controller/CalculatorController.java
@@ -22,7 +22,7 @@ public class CalculatorController {
     /* Application 상태 확인용 health check 용 메서드 */
     @GetMapping("/health")
     public String health() {
-        return "it's good!!!";
+        return "it's good!!!000";
     }
 
     /* frontend 서버에서 넘어오는 num1 , num2 를 받기 위한 메서드 */
